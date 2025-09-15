590f28c8963659d2eaf96a7fd8a16f5bf8628e42
Jeremiah Engelman <jeremiah_engelman@hotmail.com>
2025-09-14 21:14:53 -0700

    Fix for deployment to GitHub

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/HappyFunLeague/" : "/",
   plugins: [react()],
-  base: './',
-  build: { outDir: 'docs' }
-})
+  build: { outDir: "docs", assetsDir: "assets" },
+}));
