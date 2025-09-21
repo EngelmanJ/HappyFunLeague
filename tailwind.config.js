d625e62688e52b029d54704f0c3e72cfafe1bf22
Jeremiah Engelman <jeremiah_engelman@hotmail.com>
2025-09-21 15:13:14 -0700

    Next fix attempt.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "bg-slate-950", "text-slate-200", "prose", "prose-invert",
+    "border-slate-800", "bg-slate-900/60"
+  ],
   theme: { extend: {} },
   plugins: [],
 }
