4f31abcd7d22dc611cb16d2970aa4828fc5a60af
Jeremiah Engelman <jeremiah_engelman@hotmail.com>
2025-09-21 16:16:50 -0700

    Fix required updating src/index.css import of v4 Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/HappyFunLeague/" : "/",
-  plugins: [react()],
+  plugins: [react(), tailwind()],
   build: { outDir: "docs", assetsDir: "assets" },
 }));
-
-// export default defineConfig(({ command }) => ({
-//   base: command === "build" ? "./" : "/",
-//   plugins: [react()],
-//   build: { outDir: "docs", assetsDir: "assets" },
-// }));
