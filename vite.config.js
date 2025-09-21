d4e12585b224b0ddd7c3448a82b24803fa325c2f
Jeremiah Engelman <jeremiah_engelman@hotmail.com>
2025-09-21 13:21:44 -0700

    More attempts at fixing base path

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// export default defineConfig(({ command }) => ({
-//   base: command === "build" ? "/HappyFunLeague/" : "/",
-//   plugins: [react()],
-//   build: { outDir: "docs", assetsDir: "assets" },
-// }));
-
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "./" : "/",
+  base: command === "build" ? "/HappyFunLeague/" : "/",
   plugins: [react()],
   build: { outDir: "docs", assetsDir: "assets" },
 }));
+
+// export default defineConfig(({ command }) => ({
+//   base: command === "build" ? "./" : "/",
+//   plugins: [react()],
+//   build: { outDir: "docs", assetsDir: "assets" },
+// }));
